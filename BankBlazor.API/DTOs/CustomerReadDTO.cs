8d7fa567ebaa375720f7d92a184cf5be836f2d2d
Hanna Hult <hannaaaa31@gmail.com>
2025-04-17 13:30:18 +0200

    CustomerReadDTO created

---
+++ BankBlazor.API/DTOs/CustomerReadDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace BankBlazor.API.DTOs
+{
+    public class CustomerReadDTO
+    {
+        public int CustomerId { get; set; }
+        public string Givenname { get; set; } = null!;
+        public string Surname { get; set; } = null!;
+        public string? Emailaddress { get; set; }
+        public string? Telephonenumber { get; set; }
+        public string Streetaddress { get; set; } = null!;
+        public string City { get; set; } = null!;
+        public string Country { get; set; } = null!;
+    }
+}
