63b5d4863dbece50cd49d8a795898f1be44a6aa9
Hanna Hult <hannaaaa31@gmail.com>
2025-04-17 14:08:46 +0200

    Added AccountReadDTO

---
+++ BankBlazor.API/DTOs/AccountReadDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace BankBlazor.API.DTOs
+{
+    public class AccountReadDTO
+    {
+        public int AccountId { get; set; }
+        public string Frequency { get; set; }
+        public decimal Balance { get; set; }
+    }
+}
