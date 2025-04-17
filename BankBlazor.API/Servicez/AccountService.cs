1e6438eebcd78265ad6dd739b217fd36e1464ab3
Hanna Hult <hannaaaa31@gmail.com>
2025-04-17 14:13:21 +0200

    Added inerface and service for Account

---
+++ BankBlazor.API/Servicez/AccountService.cs
@@ -0,0 +1,48 @@
+﻿using BankBlazor.API.Contexts;
+using BankBlazor.API.DTOs;
+using BankBlazor.API.Servicez.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace BankBlazor.API.Servicez
+{
+    public class AccountService : IAccountService
+    {
+        private readonly BankContext _dbContext;
+
+        public AccountService(BankContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<AccountReadDTO>> GetAccountsByCustomerIdAsync(int customerId)
+        {
+            var accounts = await _dbContext.Dispositions
+                .Where(d => d.CustomerId == customerId)
+                .Include(d => d.Account)
+                .Select(d => new AccountReadDTO
+                {
+                    AccountId = d.Account.AccountId,
+                    Frequency = d.Account.Frequency,
+                    Balance = d.Account.Balance
+                })
+                .ToListAsync();
+
+            return accounts;
+        }
+        public async Task<AccountReadDTO> GetAccountByIdAsync(int accountId)
+        {
+            var account = await _dbContext.Accounts
+                .Where(a => a.AccountId == accountId)
+                .Select(a => new AccountReadDTO
+                {
+                    AccountId = a.AccountId,
+                    Frequency = a.Frequency,
+                    Balance = a.Balance
+                })
+                .FirstOrDefaultAsync();
+
+            return account;
+        }
+
+    }
+}
---
+++ BankBlazor.API/Servicez/Interfaces/IAccountService.cs
@@ -0,0 +1,10 @@
+﻿using BankBlazor.API.DTOs;
+
+namespace BankBlazor.API.Servicez.Interfaces
+{
+    public interface IAccountService
+    {
+        Task<List<AccountReadDTO>> GetAccountsByCustomerIdAsync(int customerId);
+        Task<AccountReadDTO> GetAccountByIdAsync(int accountId);
+    }
+}
