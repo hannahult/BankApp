a84c02d12ebe0c79f253a219c23dec0419966597
Hanna Hult <hannaaaa31@gmail.com>
2025-04-28 13:41:45 +0200

    Added page CustomerDashboard Modified DTO and ViewModel for customer with accounts

--- BankBlazor.API/DTOs/CustomerWithAccountsDTO.cs
+++ BankBlazor.API/DTOs/CustomerWithAccountsDTO.cs
@@ -3,8 +3,12 @@
     public class CustomerWithAccountsDTO
     {
         public int CustomerId { get; set; }
-        public string Givenname { get; set; } = null!;
-        public string Surname { get; set; } = null!;
+        public string Givenname { get; set; } = string.Empty;
+        public string Surname { get; set; } = string.Empty;
+        public string Emailaddress { get; set; } = string.Empty;
+        public string Streetaddress { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
         public List<AccountReadDTO> Accounts { get; set; } = new();
     }
 }
--- BankBlazor.Client/ViewModels/CustomerDetailsViewModel.cs
+++ BankBlazor.Client/ViewModels/CustomerDetailsViewModel.cs
@@ -5,6 +5,10 @@
         public int CustomerId { get; set; }
         public string Givenname { get; set; } = string.Empty;
         public string Surname { get; set; } = string.Empty;
+        public string Emailaddress { get; set; } = string.Empty;
+        public string Streetaddress { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
         public List<AccountReadViewModel> Accounts { get; set; } = new();
     }
 }
