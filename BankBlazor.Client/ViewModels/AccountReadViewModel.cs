4ac92de3b659aef892d5ddd4d014c2443eeda37a
Hanna Hult <hannaaaa31@gmail.com>
2025-04-28 13:20:53 +0200

    Added CustomerDetails page and view model

---
+++ BankBlazor.Client/ViewModels/AccountReadViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace BankBlazor.Client.ViewModels
+{
+    public class AccountReadViewModel
+    {
+        public int AccountId { get; set; }
+        public string Frequency { get; set; }
+        public decimal Balance { get; set; }
+        public DateOnly Created { get; set; }
+    }
+}
---
+++ BankBlazor.Client/ViewModels/CustomerDetailsViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace BankBlazor.Client.ViewModels
+{
+    public class CustomerDetailsViewModel
+    {
+        public int CustomerId { get; set; }
+        public string Givenname { get; set; } = string.Empty;
+        public string Surname { get; set; } = string.Empty;
+        public List<AccountReadViewModel> Accounts { get; set; } = new();
+    }
+}
