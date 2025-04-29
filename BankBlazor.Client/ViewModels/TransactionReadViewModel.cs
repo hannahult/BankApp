dacb0a532c31dc0814e0d0fbe5cd416aa548bd98
Hanna Hult <hannaaaa31@gmail.com>
2025-04-29 13:52:06 +0200

    Added TransactionReadViewModel Added show transactions to customer dashboard

---
+++ BankBlazor.Client/ViewModels/TransactionReadViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace BankBlazor.Client.ViewModels
+{
+    public class TransactionReadViewModel
+    {
+        public int TransactionId { get; set; }
+        public int AccountId { get; set; }
+        public DateOnly Date { get; set; }
+        public string Type { get; set; } = string.Empty;
+        public string Operation { get; set; } = string.Empty;
+        public decimal Amount { get; set; }
+        public decimal Balance { get; set; }
+        public string? Symbol { get; set; }
+        public string? Bank { get; set; }
+        public string? Account { get; set; }
+    }
+}
