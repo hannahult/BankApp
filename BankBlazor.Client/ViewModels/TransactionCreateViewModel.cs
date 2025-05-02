00b4a7c7c4baafd8648ffd85364d4c77255d6612
Hanna Hult <hannaaaa31@gmail.com>
2025-05-02 17:21:13 +0200

    Added TransactionCreateViemModel Added MakeDeposit page Added NavLinks from dashboard

---
+++ BankBlazor.Client/ViewModels/TransactionCreateViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace BankBlazor.Client.ViewModels
+{
+    public class TransactionCreateViewModel
+    {
+        public int AccountId { get; set; }
+        public decimal Amount { get; set; }
+        public string? Symbol { get; set; }
+        public string? Bank { get; set; }
+        public string? Account { get; set; }
+    }
+}
